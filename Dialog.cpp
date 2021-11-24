5bd9c84cf9c888a6ee329860bd8d12f8fbc7fb39
beyondshon <94978792+beyondshon@users.noreply.github.com>
2021-11-24 20:59:35 +0600

    Create Dialog.cpp

---
+++ Dialog.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	std::string player_name = "Артэс";
+	std::string player_name2 = "Тралл";
+	std::string x;
+	int y;
+
+
+	std::cout << "Здравствуй, как тебя зовут?" << std::endl;
+	std::cout << "Здравствуй, " << player_name << std::endl;
+
+	std::cout << "А тебя?" << std::endl;
+	std::cout << "А меня зовут " << player_name2 << std::endl;
+
+	std::cout << "Как у тебя дела?" << std::endl;
+	std::cin >> x;
+	std::cout << "У меня " << x << std::endl;
+
+	std::cout << "А у тебя?" << std::endl;
+	std::cin >> x;
+	std::cout << "У меня " << x << std::endl;
+
+	std::cout << "Кем ты работаешь?" << std::endl;
+	std::cin >> x;
+	std::cout << "Я работаю " << x << std::endl;
+
+	std::cout << "А ты кем?" << std::endl;
+	std::cin >> x;
+	std::cout << "А я работаю " << x << std::endl;
+
+	std::cout << "Сколько тебе лет?" << std::endl;
+	std::cin >> y;
+	std::cout << "Мне " << y << std::endl;
+
+	std::cout << "А тебе?" << std::endl;
+	std::cin >> y;
+	std::cout << "А мне " << y << std::endl;
